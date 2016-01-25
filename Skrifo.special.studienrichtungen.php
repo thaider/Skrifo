168071a29c054b5073c91b794625fd9a090524ff
thaider <thai@kollabor.at>
2016-01-25 23:07:49 +0100

    übertragung von vortragenden und studienrichtungen ins neue schema

--- Skrifo.special.studienrichtungen.php
+++ Skrifo.special.studienrichtungen.php
@@ -64,22 +64,22 @@ class SpecialStudienrichtungen extends SpecialPage {
 			$studienrichtungen = implode( ',', $propertyvalues );
 
 			if( strpos( $vorlage, 'Studienrichtung' ) === false && count( $propertyvalues ) > 0 ) {
-				$vorlageNeu = preg_replace( "/}}/Us", "|Studienrichtung=" . $studienrichtungen . "}}", $vorlage );
+				$vorlageNeu = preg_replace( "/}}/Us", "|Studienrichtung=" . $studienrichtungen . "}}", $vorlageNeu );
 
 				if( $par == 'go' ) {
 					$page = WikiPage::factory( $member );
 					$textNeu = str_replace( $vorlage, $vorlageNeu, $text );
 					$contentNeu = ContentHandler::makeContent( $textNeu, $member );
-					$page->doEditContent( $contentNeu, 'Bot-Update: Vortragende', EDIT_MINOR + EDIT_FORCE_BOT );
+					$page->doEditContent( $contentNeu, 'Bot-Update: Vortragende und Studienrichtungen', EDIT_MINOR + EDIT_FORCE_BOT );
 					$changes++;
 					$vorlageNeu = $vorlageNeu . ' - changed!';
 					}
 				}
+			$table .= '<tr><th colspan="2">' . $title . '</th></tr>';
+			$table .= '<tr><td>' . $vorlage . '</td><td>' . $vorlageNeu . '</td></tr>';
 			if( $changes > 50 ) {
 				break;
 				}
-			$table .= '<tr><th colspan="2">' . $title . '</th></tr>';
-			$table .= '<tr><td>' . $studienrichtungen . '</td><td>' . $vorlageNeu . '</td></tr>';
 			}
 
 		if( false ) {
@@ -89,6 +89,7 @@ class SpecialStudienrichtungen extends SpecialPage {
 			foreach( $gesamtliste as $name ) {
 				$table .= '<tr><td>' . $name . '</td></tr>';
 				}
+			$table = '<p>' . count( $gesamtliste ) . ' Vortragende</p>' . $table;
 			}
 
 		$table .= '</table>';
@@ -216,7 +217,47 @@ class SpecialStudienrichtungen extends SpecialPage {
 				'test' => '',
 				'Kamitz Reinhard' => 'Reinhard Kamitz',
 				'Loidolt Sophie' => 'Sophie Loidolt',
-				'Wladika Michael' => 'Michael Wladika'
+				'Wladika Michael' => 'Michael Wladika',
+				'Dannecker Petra' => 'Petra Dannecker',
+				'Dvorak Johann' => 'Johann Dvorak',
+				'Fabel Oliver' => 'Oliver Fabel',
+				'Fischer-Kowalski Marina' => 'Marina Fischer-Kowalski',
+				'Flecker Jörg' => 'Jörg Flecker',
+				'Flicker Eva' => 'Eva Flicker',
+				'Froschauer Ulrike' => 'Ulrike Froschauer',
+				'Hochgerner Josef' => 'Josef Hochgerner',
+				'Huber Florian' => 'Florian Huber',
+				'Huber Stefan' => 'Stefan Huber',
+				'Hudec Marcus' => 'Marcus Hudec',
+				'Hörl Josef' => 'Josef Hörl',
+				'Kafka Johanna' => 'Johanna Kafka',
+				'Klein Martina' => 'Martina Klein',
+				'Kment Patric' => 'Patric Kment',
+				'Kolland Franz' => 'Franz Kolland',
+				'Lehner Nikolaus' => 'Nikolaus Lehner',
+				'Mayrhofer-Deak Martina' => 'Martina Mayrhofer-Deak',
+				'Payrhuber Andrea' => 'Andrea Payrhuber',
+				'Pelikan Jürgen' => 'Jürgen Pelikan',
+				'Penz Otto' => 'Otto Penz',
+				'Petran Anna' => 'Anna Petran',
+				'Pfadenhauer Michaela' => 'Michaela Pfadenhauer',
+				'Reinprecht Christoph' => 'Christoph Reinprecht',
+				'Riederer Bernhard' => 'Bernhard Riederer',
+				'Schaffhauser-Linzatti Michaela' => 'Michaela Schaffhauser-Linzatti',
+				'Scheibelhofer Elisabeth' => 'Elisabeth Scheibelhofer',
+				'Schlemabach Christopher' => 'Christopher Schlembach',
+				'Sorger Gerhard' => 'Gerhard Sorger',
+				'Streitfellner Thomas' => 'Thomas Streitfellner',
+				'Stummvoll Günter' => 'Günter Stummvoll',
+				'Stögner Karin' => 'Karin Stögner',
+				'Supper Sylvia' => 'Sylvia Supper',
+				'Teitzer Roland' => 'Roland Teitzer',
+				'Verwiebe Roland' => 'Roland Verwiebe',
+				'Vettori Oliver' => 'Oliver Vettori',
+				'Weber Andreas' => 'Andreas Weber',
+				'Weilinger Arthur' => 'Arthur Weilinger',
+				'Weiß Anna' => 'Anna Weiß',
+				'Zartler-Griessl Ulrike' => 'Ulrike Zartler-Griessl'
 				);
 		$vortragende = explode( ',', $vortragende );
 		foreach( $vortragende as $key => &$vortragender ) {
